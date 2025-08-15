ce06bf8ac0ee1ddeb2b225e533fa3b7670063b6d
Hayes Barber <barberhayes@gmail.com>
2025-08-14 20:49:43 -0400

    add doc comments

--- src/TimeKeeper.h
+++ src/TimeKeeper.h
@@ -2,13 +2,49 @@
 
 #include <Arduino.h>
 
+/**
+ * @class TimeKeeper
+ * @brief A simple Arduino class to track epoch time in seconds and format time.
+ *
+ * Supports setting the current epoch time via different input types and
+ * retrieving the time in seconds or as a 12-hour formatted string.
+ */
 class TimeKeeper {
 public:
+  /**
+   * @brief Construct a new TimeKeeper instance.
+   */
   TimeKeeper();
+
+  /**
+   * @brief Set the epoch time in seconds from a C-style string.
+   * @param epochTimeSeconds Null-terminated string representing seconds since
+   * Unix epoch.
+   */
   void setEpochSeconds(const char *epochTimeSeconds);
+
+  /**
+   * @brief Set the epoch time in seconds from an unsigned long.
+   * @param epochTimeSeconds Epoch time in seconds.
+   */
   void setEpochSeconds(const unsigned long epochTimeSeconds);
+
+  /**
+   * @brief Set the epoch time in seconds from an Arduino String.
+   * @param epochTimeSeconds Epoch time as a String.
+   */
   void setEpochSeconds(const String epochTimeSeconds);
+
+  /**
+   * @brief Get the current epoch time in seconds.
+   * @return Current epoch time in seconds.
+   */
   unsigned long getEpochSeconds();
+
+  /**
+   * @brief Get the current time formatted as 12-hour string with AM/PM.
+   * @return String formatted as "HH:MM AM/PM".
+   */
   String getTime12Hour();
 
 private:
